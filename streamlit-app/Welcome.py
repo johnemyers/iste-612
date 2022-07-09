854f168e8ad1579fef48f8aed49025d0c4c374d6
John Myers <johnemyers@gmail.com>
2022-07-09 13:41:48 -0400

    Uploads are working.  Need to finalize processing.

--- streamlit-app/Welcome.py
+++ streamlit-app/Welcome.py
@@ -2,11 +2,21 @@ import streamlit as st
 from PIL import Image
 
 st.sidebar.image( "./images/home.png", use_column_width=True)
-st.sidebar.write('<style>body { margin: 0; font-family: Arial, Helvetica, sans-serif;} .header{padding: 10px 16px; background: #555; color: #f1f1f1; position:fixed;top:0;} .sticky { position: fixed; top: 0; width: 100%;} </style><div class="header" id="myHeader">FOUO Document Analysis</div>', unsafe_allow_html=True)
+st.sidebar.write('<style>body { margin: 0; font-family: Arial, Helvetica, sans-serif;} .header{padding: 10px 16px; background: #555; color: #f1f1f1; position:fixed;top:0;} .sticky { position: fixed; top: 0; width: 100%;} </style><div class="header" id="myHeader">FOIA Document Analysis</div>', unsafe_allow_html=True)
+
+title_alignment="""
+<style>
+#team-ufo {
+  text-align: center
+}
+</style>
+"""
+st.markdown(title_alignment, unsafe_allow_html=True)
 
 colT1,colT2,colT3 = st.columns(3)
 with colT2:
-  st.title("Team UFO\nFOUO Document Analysis")
+  st.title("Team UFO")
+  st.markdown( "<p style=\"text-align: center;\">FOIA Document Analysis</p>", unsafe_allow_html=True)
 with colT1:
   ufoImg = Image.open('./images/favpng_roswell-unidentified-flying-object-sprite.png')
   st.image(ufoImg, width=200)
--- streamlit-app/pages/02_Query_Data.py
+++ streamlit-app/pages/02_Query_Data.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.sidebar.image( "./images/query.png", use_column_width=True)
-st.sidebar.write('<style>body { margin: 0; font-family: Arial, Helvetica, sans-serif;} .header{padding: 10px 16px; background: #555; color: #f1f1f1; position:fixed;top:0;} .sticky { position: fixed; top: 0; width: 100%;} </style><div class="header" id="myHeader">FOUO Document Analysis</div>', unsafe_allow_html=True)
+st.sidebar.write('<style>body { margin: 0; font-family: Arial, Helvetica, sans-serif;} .header{padding: 10px 16px; background: #555; color: #f1f1f1; position:fixed;top:0;} .sticky { position: fixed; top: 0; width: 100%;} </style><div class="header" id="myHeader">FOIA Document Analysis</div>', unsafe_allow_html=True)
 
 style = open( "./pages/html/style.html")
 st.markdown( style.read(), unsafe_allow_html=True )
