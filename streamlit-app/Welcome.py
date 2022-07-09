68c18528e5ae531e379f7f6cc374519efc6b8f4c
John Myers <johnemyers@gmail.com>
2022-07-09 11:48:04 -0400

    Broke out the content into separate markdown for ease of editing.

--- streamlit-app/Welcome.py
+++ streamlit-app/Welcome.py
@@ -7,8 +7,15 @@ with colT2:
   st.title("Team UFO")
 with colT1:
   ufoImg = Image.open('./images/favpng_roswell-unidentified-flying-object-sprite.png')
-  st.image(ufoImg, width=300)
+  st.image(ufoImg, width=200)
 with colT3:
   tsImg = Image.open('./images/favpng_united-states-youtube-clip-art.png')
-  st.image( tsImg, width=300 )
+  st.image( tsImg, width=275 )
 
+intro = open( "./content/intro.md")
+
+sections = open( "./content/sections.md")
+
+st.markdown( intro.read() )
+
+st.markdown( sections.read() )
---
+++ streamlit-app/content/sections.md
@@ -0,0 +1,7 @@
+Our app is composed of three main sections:
+
+1.  *Process Data.* Import and process the PDF documents, returning basic statistics.
+2.  *Query Data.* Find a term or keyword using a configurable State Vector Model.
+3.  *Explore Data.* A final thing.
+
+These can be navigated to using the links on the left.
